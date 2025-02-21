9553ad20cd7945885bacf08e199e3ad14e397b02
MHinson0348 <madison@cni-inc.com>
2025-02-21 07:44:33 -0500

    Create side-tab-create-anchor.js

---
+++ side-tab-create-anchor.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const signupSection = document.querySelector('.section-subscribe');
+    if (signupSection) {
+        console.log('Found signup section');
+    } else {
+        console.log('Did not find subscribe section');
+    }
+});
